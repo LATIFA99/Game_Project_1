d02e90ada01509a93f54dad3bba858f87548f760
LATIFA99 <33892443+LATIFA99@users.noreply.github.com>
2020-12-14 12:24:20 +0300

    Add files via upload

---
+++ Assets/GameControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+
+{
+    [SerializeField]
+    private Transform[] landpreps;
+
+    [SerializeField]
+    private GameObject winText;
+
+    public static bool youWin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winText.SetActive(false);
+        youWin = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(landpreps[0].rotation.z == 0&&
+            landpreps[1].rotation.z == 0 &&
+            landpreps[2].rotation.z == 0 &&
+            landpreps[3].rotation.z == 0 &&
+            landpreps[4].rotation.z == 0 &&
+            landpreps[5].rotation.z == 0)
+        {
+            youWin = true;
+            winText.SetActive(true);
+        }
+    }
+}
